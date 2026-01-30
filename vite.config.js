6e2ba7bf4241ace5cceebb7ec35f047fa68ea8dc
adnansattar80@gmail.com <adnansattar80@gmail.com>
2026-01-30 21:26:43 +0100

    feat: update Vite configuration to set base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 // const path = require('path')
 
 export default {
-  // root: path.resolve(__dirname, 'src'),
+  base: "/Frontend-PWA/",
+
   resolve: {
     // alias: {
     //   '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     // }
   },
+
   // server: {
   //   port: 8080,
   //   hot: true
