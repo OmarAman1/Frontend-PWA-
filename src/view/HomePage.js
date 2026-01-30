7b6a5cec8426dcbd09d0627d42ed166c83e70b76
adnansattar80@gmail.com <adnansattar80@gmail.com>
2026-01-30 21:37:14 +0100

    feat: refactor Home component structure and add initial swapi API file

--- src/view/HomePage.js
+++ src/view/HomePage.js
@@ -1,15 +1,12 @@
-import MoviesCard from "./moviesCard";
-import NavBar from "./NavbarPage";
+import NavBar from "../Components/NavbarPage.js";
 
-export default function Home(){
-
-  return(
-    `${NavBar()}
-    <main class= "container">
-     ${MoviesCard()}
-      <h2>Footer</h2>
-      </main>
-    `
-  )
+export default function Home() {
+  return `
+    ${NavBar()}
+    <main class="container my-4">
+      <h1 class="mb-3">Galactic Dex</h1>
+      <p class="text-muted">Laddar karaktärer...</p>
+      <div id="appContent"></div>
+    </main>
+  `;
 }
-
