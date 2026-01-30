5448918cafb764b2b035fb78aade4f1cd05b6a63
David Alrefai <david.alrefai@chasacademy.se>
2026-01-30 22:50:05 +0100

    feat: add rating functionality and update MoviesCard component with star ratings

--- src/main.js
+++ src/main.js
@@ -13,3 +13,29 @@ document.querySelector("#moviesCard").addEventListener("click", (e) => {
   icon.classList.toggle("bi-heart");
   icon.classList.toggle("bi-heart-fill");
 });
+
+document.querySelector(".navbar-toggler").addEventListener("click", (e) => {
+  const navContent = document.querySelector("#navbarSupportedContent");
+  if (navContent.classList.contains("show")) {
+    navContent.classList.remove("show");
+  } else {
+    navContent.classList.add("show");
+  }
+});
+
+function handleStarClick(event) {
+  if (event.target.classList.contains("star")) {
+    const selectedValue = event.target.getAttribute("data-value");
+    const stars = event.target.parentElement.querySelectorAll(".star");
+    stars.forEach((star) => {
+      if (star.getAttribute("data-value") <= selectedValue) {
+        star.style.color = "#ffd700"; // Highlight color
+      } else {
+        star.style.color = "#d7d7d7"; // Default color
+      }
+    });
+  }
+}
+
+document.addEventListener("click", handleStarClick);
+
--- src/view/moviesCard.js
+++ src/view/moviesCard.js
@@ -1,3 +1,4 @@
+import ratingPage from "./ratingPage";
 
 
 
@@ -13,8 +14,8 @@ export default function MoviesCard(){
         <h5 class="card-title">Card title</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
      <a href="#" class="btn btn-primary">Go somewhere</a>
-
-    </div>
+      ${ratingPage()}
+      </div>
   </div>
   </div>`
 
