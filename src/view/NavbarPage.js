5b2dcde52edb25be90a3a45209dfad9765ad9943
David Alrefai <david.alrefai@chasacademy.se>
2026-02-02 22:47:43 +0100

    fix: added new style to navbar and fix searchbar

--- src/view/NavbarPage.js
+++ src/view/NavbarPage.js
@@ -1,38 +1,33 @@
 
-export default function NavBar(){
+function escapeHtml(s = "") {
+  return String(s)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
 
-return(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+export default function NavBar(activePage = "home", searchQuery = ""){
+  const isActive = (page) => (activePage === page ? "active" : "");
+  const isCurrent = (page) => (activePage === page ? 'aria-current="page"' : "");
+
+return(`<nav class="navbar">
+  <div class="container-fluid movie-nav">
+    <a class="navbar-brand" href="?page=home">Harry Potter Epics</a>
+    <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+    <div class="navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link ${isActive("home")}" ${isCurrent("home")} href="?page=home">Home</a></li>
+        <li class="nav-item"><a class="nav-link ${isActive("movies")}" ${isCurrent("movies")} href="?page=home">Movies</a></li>
+        <li class="nav-item"><a class="nav-link ${isActive("top-rated")}" ${isCurrent("top-rated")} href="?page=top-rated">Top Rated</a></li>
+        <li class="nav-item"><a class="nav-link ${isActive("favorites")}" ${isCurrent("favorites")} href="?page=favorites">Favorites</a></li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="nav-search" data-search-form>
+        <input class="form-control" name="q" type="search" placeholder="Search movie..." aria-label="Search" value="${escapeHtml(searchQuery)}">
+        <button class="btn-search" type="submit">Search</button>
       </form>
     </div>
   </div>
