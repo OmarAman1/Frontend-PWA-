97802177106460290ba7188d7ec9fe6a8cd2bd6d
David Alrefai <david.alrefai@chasacademy.se>
2026-02-12 08:40:55 +0100

    refactor: Clean up vite.config.js by removing commented-out code

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,8 @@
 
 export default {
   base: "/Frontend-PWA/",
-
-  resolve: {
-    // alias: {
-    //   '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    // }
-  },
-
-  // server: {
-  //   port: 8080,
-  //   hot: true
-  // }
+  server: {
+    port: 8080,
+    hot: true
+  }
 }
